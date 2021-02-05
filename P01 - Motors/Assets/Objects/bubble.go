9903e7c4f136d2e1b55f696f1132574c01e43f49
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2021-02-05 20:54:03 +0100

    Update

---
+++ P01 - Motors/Assets/Objects/bubble.go
@@ -0,0 +1,35 @@
+components {
+  id: "bubble"
+  component: "/Assets/GUI/bubble.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bubbleSprite"
+  type: "sprite"
+  data: "tile_set: \"/Assets/GUI/Atlas.atlas\"\n"
+  "default_animation: \"bubble\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
