495a97c5af858e16b25557d601b704c2c482acf7
JoanStark <47462893+JoanStark@users.noreply.github.com>
2021-02-05 15:14:00 +0100

    00

---
+++ P01 - Motors/Assets/GUI/GUI.go
@@ -0,0 +1,62 @@
+components {
+  id: "ClickPercentatgeNode"
+  component: "/Assets/GUI/GameGUI.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "GUIProperties"
+  component: "/Assets/GUI/GUIProperties.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "fillAt"
+    value: "15.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "fillAnimationTime"
+    value: "1.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
+embedded_components {
+  id: "ClickSound"
+  type: "sound"
+  data: "sound: \"/Assets/ClickManager/Pop Bubble.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
