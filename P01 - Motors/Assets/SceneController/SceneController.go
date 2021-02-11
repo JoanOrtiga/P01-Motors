575e2d209570fac9ec9cd8f2a50fd76559c53fa8
JoanStark <47462893+JoanStark@users.noreply.github.com>
2021-02-11 11:37:56 +0100

    LevelSwitcher

---
+++ P01 - Motors/Assets/SceneController/SceneController.go
@@ -0,0 +1,51 @@
+components {
+  id: "SceneController"
+  component: "/Assets/SceneController/SceneController.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "mainMenu"
+  type: "collectionproxy"
+  data: "collection: \"/main/MainMenu.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level1"
+  type: "collectionproxy"
+  data: "collection: \"/main/main.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
